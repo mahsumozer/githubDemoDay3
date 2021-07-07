94e15ed412abcd4142b4df61b14ad32211c07a62
Mahsum OZER <46961602+mahsumozer@users.noreply.github.com>
2021-07-07 17:00:47 +0300

    static block

--- src/class1/try1.java
+++ src/class1/try1.java
@@ -4,8 +4,13 @@ interface a{
     void tryy(); // this is already abstract wwe can just delete " abstract "
         }
 
-
 public class try1 implements a {
+    static{
+        System.out.println("static block start ...");
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("try try try");
     }
